4ce569afa75fcae1189987139dbafbd0388492b8
amirmohammadbahreini <amirarmaneh@gmail.com>
2024-09-14 22:04:12 +0330

    Add files via upload

---
+++ amir.py
@@ -0,0 +1,31 @@
+x = 0.3
+y = str(x)
+print(id(x))
+print(id(y))
+print(x==y)
+
+import fractions
+print(fractions.Fraction(2**-100))
+
+a = -2e+1402
+print(a)
+
+x = 10
+print(isinstance(x, int))   # x az noa dovomi ast ya na
+
+my_str = "AMIr" "" "ss"
+print(my_str)
+
+
+#\r edame matn
+#\a hich kari nmi konad
+#\f alamat mard
+#\v alamat zan kaj
+#\1 @
+#\2 khande
+#\3 ghalb
+#\12 khatbad
+del a 
+number = [1000000000, 1000000000, 1000000000, 1000000000, 1000000000]
+a = ['_']
+print(a.join(number))
